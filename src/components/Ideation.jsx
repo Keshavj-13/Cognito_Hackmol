89f5885972fbf427e14562fe394dae066a09ff7c
SHIVANSH-1901 <shivansh19srivastava@gmail.com>
2025-04-10 16:31:05 +0530

    CHANGING NAV HOVER COLOR TO AMBER

---
+++ src/components/Ideation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import myImage from './path/to/your/image.jpg'; // Update with the actual path to your image
+
+const MyComponent = () => {
+    return (
+        <div>
+            <img src={myImage} alt="Description" />
+        </div>
+    );
+};
+
+export default MyComponent;
